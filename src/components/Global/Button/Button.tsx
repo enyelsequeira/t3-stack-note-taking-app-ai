c4f05b6c908633fcece91d317141782c9f1c181f
EnyelSequeira <enyelsequeira@hotmail.com>
2023-01-19 16:22:08 +0000

    chore: adding a small description about giving credits

--- src/components/Global/Button/Button.tsx
+++ src/components/Global/Button/Button.tsx
@@ -3,7 +3,7 @@ import { cva } from "cva";
 import type { LinkProps } from "next/link";
 import Link from "next/link";
 import React, { forwardRef } from "react";
-import { applyStyleToMultipleVariants, classNames } from "../CVA";
+import { classNames } from "../CVA";
 
 type InferredVariantProps = VariantProps<typeof buttonClasses>;
 export type ButtonColor = NonNullable<InferredVariantProps["color"]>;
@@ -45,7 +45,6 @@ const buttonClasses = cva(
         base: "h-9 px-4 py-2.5 ",
         lg: "h-[36px] px-4 py-2.5 ",
         icon: "flex justify-center min-h-[36px] min-w-[36px] ",
-        // fab = floating action button, used for the main action in a page.
         // it uses the same primary classNames for desktop size
         fab: "h-14 w-14 sm:h-9 sm:w-auto rounded-full justify-center sm:rounded-md sm:px-4 sm:py-2.5 radix-state-open:rotate-45 sm:radix-state-open:rotate-0 transition-transform radix-state-open:shadown-none radix-state-open:ring-0 !shadow-none",
       },
@@ -72,90 +71,15 @@ const buttonClasses = cva(
           "inline-block text-black rounded-lg px-4 py-1.5 text-base font-semibold leading-7  ring-1 ring-gray-900/10 hover:ring-gray-900/20",
       },
     },
-    compoundVariants: [
-      // Primary variants
-      {
-        disabled: true,
-        color: "primary",
-        className:
-          "bg-gray-800 bg-opacity-30 dark:bg-opacity-30 dark:bg-darkgray-800",
-      },
-      {
-        loading: true,
-        color: "primary",
-        className:
-          "bg-gray-800/30 text-white/30 dark:bg-opacity-30 dark:bg-darkgray-700 dark:text-black/30",
-      },
-      ...applyStyleToMultipleVariants({
-        disabled: [undefined, false],
-        color: "primary",
-        className:
-          "bg-brand-500 hover:bg-brand-400 focus:outline-none focus:ring-2 focus:ring-offset focus:ring-brand-500 dark:hover:bg-darkgray-600 dark:bg-darkgray-900",
-      }),
-      // Secondary variants
-      {
-        disabled: true,
-        color: "secondary",
-        className:
-          "border border-gray-200 bg-opacity-30 text-gray-900/30 dark:text-darkgray-900/30 dark:border-darkgray-200",
-      },
-      {
-        loading: true,
-        color: "secondary",
-        className:
-          "bg-gray-100 text-gray-900/30 dark:bg-darkgray-100 dark:text-darkgray-900/30 dark:border-darkgray-200",
-      },
-      ...applyStyleToMultipleVariants({
-        disabled: [undefined, false],
-        color: "secondary",
-        className:
-          "border border-gray-300 dark:border-darkgray-300 bg-white dark:bg-darkgray-100 hover:bg-gray-50 hover:border-gray-400 focus:bg-gray-100 dark:hover:bg-darkgray-200 dark:focus:bg-darkgray-200 focus:outline-none focus:ring-2 focus:ring-offset focus:ring-gray-900 dark:focus:ring-white",
-      }),
-      // Minimal variants
-      {
-        disabled: true,
-        color: "minimal",
-        className:
-          "border:gray-200 bg-opacity-30 text-gray-900/30 dark:bg-darkgray-100 dark:text-darkgray-900/30 dark:border-darkgray-200",
-      },
-      {
-        loading: true,
-        color: "minimal",
-        className:
-          "bg-gray-100 text-gray-900/30 dark:bg-darkgray-100 dark:text-darkgray-900/30 dark:border-darkgray-200",
-      },
-      ...applyStyleToMultipleVariants({
-        disabled: [undefined, false],
-        color: "minimal",
-        className:
-          "hover:bg-gray-100 focus:bg-gray-100 dark:hover:bg-darkgray-200 dark:focus:bg-darkgray-200 focus:outline-none focus:ring-2 focus:ring-offset focus:ring-gray-900 dark:focus:ring-white",
-      }),
-      // Destructive variants
-      {
-        disabled: true,
-        color: "destructive",
-        className:
-          "text-red-700/30 dark:text-red-700/30 bg-red-100/40 dark:bg-red-100/80 border border-red-200",
-      },
-      {
-        loading: true,
-        color: "destructive",
-        className:
-          "text-red-700/30 dark:text-red-700/30 hover:text-red-700/30 bg-red-100 border border-red-200",
-      },
-      ...applyStyleToMultipleVariants({
-        disabled: [false, undefined],
-        color: "destructive",
-        className:
-          "border dark:text-white text-gray-900 hover:text-red-700 focus:text-red-700 dark:hover:text-red-700 dark:focus:text-red-700 hover:border-red-100 focus:border-red-100 hover:bg-red-100  focus:bg-red-100 focus:outline-none focus:ring-2 focus:ring-offset focus:ring-red-700",
-      }),
-    ],
+
     defaultVariants: {
       color: "primary",
       size: "base",
     },
   }
 );
+// this was taken as inspiration from
+// https://github.com/calcom/cal.com/tree/main/packages/lib/cva
 
 export const Button = forwardRef<
   HTMLAnchorElement | HTMLButtonElement,
