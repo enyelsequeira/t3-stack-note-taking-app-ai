f06bdbecef0cac699b99c04b75d554b21af6badb
EnyelSequeira <enyelsequeira@hotmail.com>
2023-05-02 19:27:36 +0100

    feat: adding editing mode

--- src/pages/posts/post/[post].tsx
+++ src/pages/posts/post/[post].tsx
@@ -3,12 +3,12 @@ import { trpc } from "../../../utils/trpc";
 import Layout from "@/layout";
 import Box from "@/components/Global/Box/Box";
 import { useRouter } from "next/router";
-import { Button, Skeleton } from "@mantine/core";
+import { Button, Skeleton, Text } from "@mantine/core";
 import { OwnInput } from "@/components/Form/Input";
 import useZodForm from "@/hooks/use-zod-form";
 import { CreateNote } from "@/schemas/validations";
 import { OwnMultiSelect } from "@/components/Form/MultiSelect";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import Editor from "@/components/Editor";
 import { Controller } from "react-hook-form";
 import { TRPCClientError } from "@trpc/client";
@@ -19,7 +19,10 @@ import { useSession } from "next-auth/react";
 const Post = () => {
   const router = useRouter();
   const { data: userData } = useSession();
-
+  const [keywords, setKeywords] = useState<{ value: string; label: string }[]>(
+    []
+  );
+  const ref = useRef();
   const { data, isLoading, error } = trpc.post.getById.useQuery(
     {
       id: router.query.post as string,
@@ -39,9 +42,19 @@ const Post = () => {
   });
   console.log({ router });
 
-  if (isLoading) {
-    return <Skeleton className="mt-7" visible={isLoading}></Skeleton>;
-  }
+  useEffect(() => {
+    if (!isLoading) {
+      if (data && data.keywords) {
+        setKeywords(
+          data.keywords.map((keyword) => ({
+            value: keyword,
+            label: keyword,
+          }))
+        );
+      }
+    }
+  }, [isLoading, data]);
+
   const handleDoneClick = () => {
     if (router.asPath) {
       const newPath = (router.asPath ?? "").split("?")[0];
@@ -50,68 +63,49 @@ const Post = () => {
       }
     }
   };
-
+  if (isLoading) {
+    return <Skeleton className="mt-7" visible={isLoading}></Skeleton>;
+  }
   return (
     <Layout>
       <Box as="section" className="relative px-6 lg:px-8">
-        <Button
-          variant="outline"
-          // when we click we should remove the ?edit=true from the path
-          // onClick={handleDoneClick}
-          onClick={() => {
-            mutate(
-              {
-                keywords: watch("keywords"),
-                text: watch("text"),
-                title: watch("title"),
-                userId: userData?.user?.id as string,
-                postId: data?.id as string,
-              },
-              {
-                onError: (error) => {
-                  console.log({ error });
-                  if (error instanceof TRPCClientError) {
-                    notifications.show({
-                      icon: <IconError404 />,
-                      message: error.message,
-                    });
-                  } else {
-                    notifications.show({
-                      icon: <IconError404 />,
-                      message: "Something went wrong with Post Creation",
-                    });
-                  }
-                },
-                onSuccess: () => {
-                  notifications.show({
-                    message: "Post Updated",
-                    title: "Success",
-                  });
-                  handleDoneClick();
-                },
-              }
-            );
-          }}
+        <Text
+          variant="gradient"
+          gradient={{ from: "red", to: "blue", deg: 45 }}
+          fw={700}
+          className="pt-10 text-2xl"
         >
-          DONE
-        </Button>
-        <p>{router.query?.edit ? "edit Mode" : "VIEW MODE"}</p>
-        <Box container>
+          {router.query?.edit ? "Edit Mode" : "Reading  Mode"}
+        </Text>
+        <Box container className="flex flex-col gap-y-6">
           <OwnInput
             control={control}
             name="title"
             placeholder={data?.title}
             readOnly={!router.query?.edit}
+            label="Title"
+            classNames={{
+              label: "mb-2 text-xl",
+            }}
           />
           <OwnMultiSelect
+            classNames={{
+              label: "mb-2 text-lg",
+            }}
             control={control}
-            readOnly={!router.query?.edit}
             name="keywords"
-            data={[]}
+            data={keywords}
             creatable
+            searchable
             getCreateLabel={(query) => `+ Create ${query}`}
             label="Choose 3 tags or create new"
             placeholder={data?.keywords?.join(", ")}
+            maxSelectedValues={5}
+            onCreate={(query) => {
+              const item = { value: query, label: query };
+              setKeywords((current) => (current ? [...current, item] : [item]));
+              return item;
+            }}
           />
           <Controller
             render={({ field }) => (
@@ -124,6 +118,46 @@ const Post = () => {
             name="text"
             control={control}
           />
+          <Button
+            variant="outline"
+            my={"md"}
+            onClick={() => {
+              mutate(
+                {
+                  keywords: watch("keywords"),
+                  text: watch("text"),
+                  title: watch("title"),
+                  userId: userData?.user?.id as string,
+                  postId: data?.id as string,
+                },
+                {
+                  onError: (error) => {
+                    console.log({ error });
+                    if (error instanceof TRPCClientError) {
+                      notifications.show({
+                        icon: <IconError404 />,
+                        message: error.message,
+                      });
+                    } else {
+                      notifications.show({
+                        icon: <IconError404 />,
+                        message: "Something went wrong with Post Creation",
+                      });
+                    }
+                  },
+                  onSuccess: () => {
+                    notifications.show({
+                      message: "Post Updated",
+                      title: "Success",
+                    });
+                    handleDoneClick();
+                  },
+                }
+              );
+            }}
+          >
+            Finish Editing
+          </Button>
         </Box>
       </Box>
     </Layout>
